d4fcea6b42900d882f0fa86dd9fbbac5bdc465a7
Vedant Sawant <vedantsawant2803@gmail.com>
2026-08-26 23:37:30 +0530

    feat: add PATCH endpoint for partial project updates

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -7,6 +7,7 @@ router.post('/', auth, projectController.createProject);
 router.get('/', auth, projectController.getProjects);
 router.get('/:id', auth, projectController.getProjectById);
 router.put('/:id', auth, projectController.updateProject);
+router.patch('/:id', auth, projectController.updateProject);
 router.delete('/:id', auth, projectController.deleteProject);
 
 module.exports = router;
