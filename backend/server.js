7bcda814254054a8ff67abec3cab417c335e6b18
Atharva Nitin Jadhav <atharvaj7822@gmail.com>
2026-09-14 11:17:25 +0530

    Replace express-mongo-sanitize with custom body sanitizer for Express 5

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,23 @@ const helmet = require('helmet');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
-const mongoSanitize = require('express-mongo-sanitize');
 const hpp = require('hpp');
+
+const sanitizeMongoKeys = (obj) => {
+  if (Array.isArray(obj)) {
+    obj.forEach(sanitizeMongoKeys);
+    return;
+  }
+  if (obj && typeof obj === 'object') {
+    Object.keys(obj).forEach((key) => {
+      if (key.startsWith('$') || key.includes('.')) {
+        delete obj[key];
+      } else {
+        sanitizeMongoKeys(obj[key]);
+      }
+    });
+  }
+};
 
 const app = express();
 
@@ -27,9 +42,7 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use((req, res, next) => {
-  ['body', 'params', 'query'].forEach((key) => {
-    if (req[key]) mongoSanitize.sanitize(req[key]);
-  });
+  sanitizeMongoKeys(req.body);
   next();
 });
 app.use(hpp());
